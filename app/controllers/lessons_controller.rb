26c970ef27b9a3afd2f0da254e89c733fc2bca8b
James <jguest777@gmail.com>
2019-10-22 15:12:10 +0000

    config Stripe payments

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
   end
@@ -9,4 +11,13 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_lesson
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be Enrolled in the Course to view.'
+    end
+  end
+
 end
+
+
